a11e6d68341326858515d0c98f4013860505583d
souravsen29 <souravsen740@gmail.com>
2021-07-19 09:37:56 +0530

    odule solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -10,13 +10,21 @@
 // See Lecture 52, part 1
 // var byeSpeaker =
 
+
+
+
+
+
+
+
+
+
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
